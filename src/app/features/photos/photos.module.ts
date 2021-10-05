10ee3816c70639d3f1547742d7ff1ab7816ef604
dymafr <erwan1309@hotmail.com>
2021-10-05 14:09:25 +0200

    3 files modified

--- src/app/features/photos/photos.module.ts
+++ src/app/features/photos/photos.module.ts
@@ -5,10 +5,16 @@ import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { LayoutModule } from '../../shared/modules/layout.module';
 import { PHOTOS_ROUTES } from './photos.routes';
+import { PhotosListComponent } from './views/photos-search/components/photos-list/photos-list.component';
+import { PhotosSearchbarComponent } from './views/photos-search/components/photos-searchbar/photos-searchbar.component';
 import { PhotosSearchComponent } from './views/photos-search/photos-search.component';
 
 @NgModule({
-  declarations: [PhotosSearchComponent],
+  declarations: [
+    PhotosSearchComponent,
+    PhotosSearchbarComponent,
+    PhotosListComponent,
+  ],
   imports: [
     LayoutModule,
     FormsModule,
