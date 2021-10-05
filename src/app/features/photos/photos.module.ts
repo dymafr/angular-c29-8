4dc272e8e411069f7f0848f4842b349286c5bbc9
dymafr <erwan1309@hotmail.com>
2021-10-05 13:59:26 +0200

    2 files modified, 1 added, 1 deleted

--- src/app/features/photos/photos.module.ts
+++ src/app/features/photos/photos.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { LayoutModule } from '../../shared/modules/layout.module';
+import { PHOTOS_ROUTES } from './photos.routes';
 import { PhotosSearchComponent } from './views/photos-search/photos-search.component';
 
 @NgModule({
   declarations: [PhotosSearchComponent],
-  imports: [],
+  imports: [
+    LayoutModule,
+    FormsModule,
+    RouterModule.forChild(PHOTOS_ROUTES),
+    // StoreModule.forFeature(),
+    EffectsModule.forFeature([]),
+  ],
 })
 export class PhotosModule {}
